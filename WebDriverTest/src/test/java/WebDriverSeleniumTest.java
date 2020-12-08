c79e82edb8b79e3edd52d1acdce9da5c6798e72a
Alina Sidorova <alinasidorova2812@gmail.com>
2020-12-09 01:00:21 +0300

    fix: make webdriver test stable

--- WebDriverTest/src/test/java/WebDriverSeleniumTest.java
+++ WebDriverTest/src/test/java/WebDriverSeleniumTest.java
@@ -1,17 +1,16 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class WebDriverSeleniumTest {
@@ -23,13 +22,16 @@ public class WebDriverSeleniumTest {
     final String SEARCH_XPATH = "//*[@id='header-search-form']/div/input[1]";
     final String BUTTON_XPATH="//*[@title='След.']";
     final String RESULTS_CLASSNAME = "zag";
+    final int PAGES_AMOUNT = 16;
 
     @BeforeMethod(alwaysRun = true)
     public void browserSetUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-gpu");
         options.setProxy(null);
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
         driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
     }
 
     @AfterMethod(alwaysRun = true)
@@ -38,7 +40,7 @@ public class WebDriverSeleniumTest {
         driver = null;
     }
 
-    @Test (description = "test search results to match the correct query")
+    @Test (description = "test search results to match the correct query", invocationCount = 3)
     public void commonSearchTermResultsMatchCorrectQuery() {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
@@ -47,8 +49,12 @@ public class WebDriverSeleniumTest {
         new WebDriverWait(driver, 10)
                 .until(CustomConditions.jQueryAjaxCompleted());
 
+        new WebDriverWait(driver, 15)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='promo']")));
+
         WebElement form = waitForElementLocatedBy(driver, By.id(FORM_ID));
         form.click();
+
         WebElement searchInput = waitForElementLocatedBy(driver, By.xpath(SEARCH_XPATH));
         searchInput.sendKeys(CORRECT_QUERY + "\n");
 
@@ -57,16 +63,9 @@ public class WebDriverSeleniumTest {
                 .stream()
                 .map(s -> s.getAttribute("title").toLowerCase())
                 .collect(Collectors.toList());
-        List<WebElement> pages = driver.findElements(By.xpath("/html/body/section/div/div[2]/div[2]/section/div/ul/li/a"));
-        int amountOfPages = Integer.parseInt(pages.get(pages.size() - 1).getText());
 
-        for (int i = 1; i < amountOfPages; i++) {
-            List<WebElement> arrow = waitForElementsLocatedBy(driver, By.xpath(BUTTON_XPATH));
-            if (arrow.size() == 1) {
-                arrow.get(0).click();
-            } else {
-                arrow.get(1).click();
-            }
+        for (int i = 1; i < PAGES_AMOUNT; i++) {
+            waitForElementLocatedBy(driver, By.xpath(BUTTON_XPATH)).click();
             searchResults
                     .addAll(waitForElementsLocatedBy(driver, By.className(RESULTS_CLASSNAME))
                             .stream()
@@ -75,20 +74,22 @@ public class WebDriverSeleniumTest {
         }
 
         System.out.println("Search results number: " + searchResults.size());
-        Assert.assertTrue(
-                searchResults
-                        .stream()
-                        .allMatch(s -> s.contains(CORRECT_QUERY))
-        );
+        Assert.assertTrue(searchResults.stream().allMatch(s -> s.contains(CORRECT_QUERY)));
     }
 
     private static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
-        return new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.presenceOfElementLocated(by));
+        return new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofMillis(250))
+                .withMessage("Element was not found")
+                .until(ExpectedConditions.elementToBeClickable(by));
     }
 
     private static List<WebElement> waitForElementsLocatedBy(WebDriver driver, By by) {
-        return new WebDriverWait(driver, 10)
+        return new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofMillis(250))
+                .withMessage("Elements were not found")
                 .until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
     }
 }
